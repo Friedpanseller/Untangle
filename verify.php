749427309349b210a0fae3375d515bb972c1f05f
Friedpanseller <friedpanseller@gmail.com>
2017-07-26 13:40:04 +1000

    Fixed verify.php
    Fixed verify.php by changing ID to zID and updating verified = 1 if verified

--- verify.php
+++ verify.php
@@ -7,13 +7,20 @@
     $link = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
     $search = $_GET["user"];
-    $stmt = $link->prepare("SELECT code FROM users WHERE ID = ?;");
+    $stmt = $link->prepare("SELECT code FROM users WHERE zID = ?;");
     $stmt->bindParam(1,$search);
 
     if($stmt->execute()) {
         $rows = $stmt->fetchAll();
         if($rows) {
-            echo $rows["code"]
+            foreach($rows as $row) {
+                if($row["code"] == $_GET["code"]) {
+                    echo "Account Verified!";
+                     $stmt = $link->prepare("UPDATE users SET verified=1 WHERE zID='" . $_GET["user"] . "';");
+                     $stmt->execute();
+                }
+                break;
+            }
         } else {
             echo "<br /><br /><center>No Results Found</center>";
         }
